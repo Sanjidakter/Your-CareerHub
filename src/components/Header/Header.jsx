000f21c851caab9b0fe0ce8981e1b64df0bd5254
Sanjida Akter <sanjidakter2304@gmail.com>
2023-04-09 23:25:40 +0600

    nav section done

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <nav className='header'>
+             <h2>GetHired</h2>
+            <div className='nav-links'>
+            <Link to="/">Home</Link>
+            <Link to="/statistics">Statistics</Link>
+            <Link to="/appliedjobs">Applied Jobs</Link>
+            <Link to="/blog">Blog</Link>
+            </div>
+            <button className='btn-apply'>Start Applying</button>
+        </nav>
+    );
+};
+
+export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,35 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './components/Home/Home';
+import AppliedJobs from './components/AppliedJobs/AppliedJobs';
+import Main from './components/Layout/Main';
+
+const router = createBrowserRouter(
+  [
+    {
+      path:'/',
+      element: <Main></Main>,
+      children:[
+        {
+          path: '/',
+          element: <Home></Home>
+        },
+        {
+          path: 'appliedjobs',
+          element: <AppliedJobs></AppliedJobs>
+        }
+      ]
+    }
+  ]
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+     <RouterProvider router={router} />
   </React.StrictMode>,
 )
